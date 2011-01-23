fbdaa74273c2b2be961cfac449834807a5fb9b20
rharagutchi@gmail.com <rharagutchi@gmail.com@d64d5008-a3b7-dd0f-ed54-6a396d1719e5>
2011-01-23 23:55:52 +0000

    made improvements to moving the overlay image after taking a picture

--- trunk/Spherorama/src/edu/purdue/spherorama/Sphere.java
+++ trunk/Spherorama/src/edu/purdue/spherorama/Sphere.java
@@ -3,13 +3,55 @@ package edu.purdue.spherorama;
 public class Sphere {
 	
 	private String [][] tiles;
+	private int level;
+	private int location;
 	
 	public Sphere() {
 		tiles = new String[5][];
 		tiles[0] = new String[1];
-		tiles[1] = new String[4];
-		tiles[2] = new String[7];
-		tiles[3] = new String[4];
+		tiles[1] = new String[6];
+		tiles[2] = new String[6];
+		tiles[3] = new String[6];
 		tiles[4] = new String[1];
+		level = 2;
+		location = 0;
+	}
+	
+	/**
+	 * Used to keep track of which location in the sphere we are in
+	 * @param direction 0 for up, 1 for right, 2 for down, 3 for left
+	 * @return true if that direction is allowed, false otherwise (i.e. can't 
+	 * 		   move up if we're already at level 0)
+	 */
+	public boolean changeLocation(int direction) {
+		switch(direction) {
+		case 0:
+			if (level == 0)
+				return false;
+			level++;
+			if (level == 0)
+				location = 0;
+			break;
+		case 1:
+			if (level == 0 || level == 4)
+				location = 0;
+			else 
+				location = (location+1)%6;
+			break;
+		case 2:
+			if (level == 4)
+				return false;
+			level--;
+			if (level == 4)
+				location = 0;
+			break;
+		case 3:
+			if (level == 0 || level == 4)
+				location = 0;
+			else 
+				location = (location-1)%6;
+			break;
+		}
+		return true;		
 	}
 }
